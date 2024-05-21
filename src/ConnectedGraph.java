0e5942aa375365077fd4f7569464f9ae4abd063a
Ryz Clyd Sabucido <rcsabucido@gmail.com>
2024-05-21 19:47:10 +0800

    Add to java class

--- src/ConnectedGraph.java
+++ src/ConnectedGraph.java
@@ -1,5 +1,20 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
 public class ConnectedGraph {
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
 
+        System.out.println("Enter the number of vertices: ");
+        int n = sc.nextInt();
+        String[] edgeList = new String[n];
+
+        for (int i = 0; i < edgeList.length; i++) {
+            System.out.println("Enter edge " + i + ": ");
+            edgeList[i] = sc.next();
+            sc.nextLine();
+        }
+
+        System.out.println(Arrays.toString(edgeList));
     }
 }
