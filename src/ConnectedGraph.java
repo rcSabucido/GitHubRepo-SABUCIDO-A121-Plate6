d1bcf791d74ea4fc3bd163f8ccd38c15ae9b1daf
Ryz Clyd Sabucido <rcsabucido@gmail.com>
2024-05-22 10:14:23 +0800

    Add initial logic to 1st problem class

--- src/ConnectedGraph.java
+++ src/ConnectedGraph.java
@@ -5,15 +5,8 @@ public class ConnectedGraph {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
-        System.out.println("Enter the number of vertices: ");
-        int n = sc.nextInt();
-        String[] edgeList = new String[n];
-
-        for (int i = 0; i < edgeList.length; i++) {
-            System.out.println("Enter edge " + i + ": ");
-            edgeList[i] = sc.next();
-            sc.nextLine();
-        }
+        System.out.print("Enter the edge pairs of the graph (separate by comma): ");
+        String[] edgeList = sc.nextLine().split(", ");
 
         System.out.println(Arrays.toString(edgeList));
     }
