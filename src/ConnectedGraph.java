28a40a16e1ff231b5f55b438ef830677489074e4
Ryz Clyd Sabucido <rcsabucido@gmail.com>
2024-05-22 10:21:43 +0800

    Add more logic to first problem

--- src/ConnectedGraph.java
+++ src/ConnectedGraph.java
@@ -1,13 +1,23 @@
-import java.util.Arrays;
 import java.util.Scanner;
 
 public class ConnectedGraph {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+        int connectCount = 0;
 
         System.out.print("Enter the edge pairs of the graph (separate by comma): ");
         String[] edgeList = sc.nextLine().split(", ");
 
-        System.out.println(Arrays.toString(edgeList));
+        for (int i = 0; i < edgeList.length; i++) {
+            for (int j = i + 1; j < edgeList.length; j++) {
+                if (edgeList[i].charAt(edgeList[i].length() - 1) != edgeList[j].charAt(0)) {
+                    System.out.println("Graph is not connected");
+                } else {
+                    connectCount++;
+                }
+            }
+        }
+
+        System.out.println("Number of connected graphs: " + connectCount);
     }
 }
