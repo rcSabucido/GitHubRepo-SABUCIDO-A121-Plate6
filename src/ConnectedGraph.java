09ca1709a4fd66522fca0635949286823aeb74f1
Ryz Clyd Sabucido <rcsabucido@gmail.com>
2024-05-22 20:51:46 +0800

    Add more logic to ConnectedGraph.java

--- src/ConnectedGraph.java
+++ src/ConnectedGraph.java
@@ -7,17 +7,27 @@ public class ConnectedGraph {
 
         System.out.print("Enter the edge pairs of the graph (separate by comma): ");
         String[] edgeList = sc.nextLine().split(", ");
-
+        String hold = "";
         for (int i = 0; i < edgeList.length; i++) {
             for (int j = i + 1; j < edgeList.length; j++) {
-                if (edgeList[i].charAt(edgeList[i].length() - 1) != edgeList[j].charAt(0)) {
-                    System.out.println("Graph is not connected");
-                } else {
+                System.out.println(edgeList[i] + "->" + edgeList[j]);
+                if (edgeList[i].charAt(edgeList[i].length() - 1) == edgeList[j].charAt(0)) {
+                    hold = "Graph is connected";
                     connectCount++;
+                } else if (edgeList[i].charAt(0) == edgeList[j].charAt(edgeList[j].length() - 1)) {
+                    hold = "Graph is connected";
+                    connectCount++;
+                } else if (edgeList[i].charAt(0) == edgeList[j].charAt(0)) {
+                    hold = "Graph is connected";
                 }
+                else {
+                    hold = "Graph is not connected";
+                }
+                System.out.println(hold);
             }
-        }
 
+        }
+        System.out.println(hold);
         System.out.println("Number of connected graphs: " + connectCount);
     }
 }
